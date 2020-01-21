8fb55b50ff7c1e0fd7b1317fbf51965a8d4ab257
radoslawolejniczak <r.olejniczak@asper.net.pl>
2020-01-21 22:54:34 +0100

    cleanup, fetching and base of displaing data

---
+++ src/components/PostsViewer.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+        <tr>
+          <th class="text-left">ID</th>
+          <th class="text-left">Title</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <td>{{ post.id }}</td>
+          <td>{{ post.title }}</td>
+        </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'ObjectsViewer',
+  created () {
+
+  },
+  computed: {
+    ...mapGetters({
+      posts: 'getPosts'
+    })
+  },
+  mounted () {
+    this.fetchPosts()
+  },
+  methods: {
+    ...mapActions(['fetchPosts'])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    posts: null
   },
   mutations: {
+    setPosts (state, data) {
+      state.posts = data
+    }
   },
   actions: {
+    async fetchPosts ({ commit }) {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+      if (response.ok) {
+        commit('setPosts', (await response.json()))
+      }
+    }
+  },
+  getters: {
+    getPosts (state) {
+      return state.posts
+    }
   },
   modules: {
   }
